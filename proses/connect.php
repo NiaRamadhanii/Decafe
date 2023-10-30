810249d265831900a9e00644c5913f4c515af565
NiaRamadhanii <niaimas9@gmail.com>
2023-10-30 17:06:12 +0700

    commit keempat

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect("localhost","root","","db_decafe");
+    $conn = mysqli_connect("localhost","u837952942_nia",";Fg;6h66K2","u837952942_nia");
     if(!$conn){
         echo "Gagal koneksi";
     }
